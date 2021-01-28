dc1612487ca38cc9e87cb40d83ed33984aea4088
GabrielJunqueira <64487574+GabrielJunqueira@users.noreply.github.com>
2021-01-28 19:33:48 -0300

    Add files via upload

---
+++ Decorator - POO/OpacidadeDecorator.cpp
@@ -0,0 +1,24 @@
+#include "../Decorator - POO/OpacidadeDecorator.hpp"
+
+std::string OpacidadeDecorator::desenha() {
+	if (this->_opacidade >= 0 && this->_opacidade <= 0.33)
+		return FormaDecorator::desenha() + "\n- opacidade: baixa";
+
+	if (this->_opacidade > 0.33 && this->_opacidade <= 0.66)
+		return FormaDecorator::desenha() + "\n- opacidade: média";
+
+	if (this->_opacidade > 0.66 && this->_opacidade <= 1)
+		return FormaDecorator::desenha() + "\n- opacidade: alta";
+
+	else {
+	return FormaDecorator::desenha();
+}
+}
+
+void OpacidadeDecorator::setOpacidade(double opacidade) {
+this->_opacidade = opacidade;
+}
+
+double OpacidadeDecorator::getOpacidade() {
+return this->_opacidade;
+}
---
+++ Decorator - POO/TamanhoDecorator.cpp
@@ -0,0 +1,13 @@
+#include "../Decorator - POO/TamanhoDecorator.hpp"
+
+std::string TamanhoDecorator::desenha() {
+	return FormaDecorator::desenha() + "\n- tamanho: " + std::to_string(this->_tamanho);
+}
+
+void TamanhoDecorator::setTamanho(int tamanho) {
+	this->_tamanho = tamanho;
+}
+
+int TamanhoDecorator::getTamanho() {
+	return this->_tamanho;
+}
